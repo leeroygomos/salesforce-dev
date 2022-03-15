7a55440ca0bbe94d0ff4554a1671838373e021ed
Lee Roy Gomos <leeroy.gomos@gmail.com>
2022-03-15 01:19:35 -0700

    updates to portfolio

--- force-app/main/default/lwc/projects/projects.js
+++ force-app/main/default/lwc/projects/projects.js
@@ -1,3 +1,11 @@
 import { LightningElement } from 'lwc';
+import PROJECT_IMAGES from '@salesforce/resourceUrl/projectImages';
 
-export default class Projects extends LightningElement {}
+export default class Projects extends LightningElement {
+    fitnessBuddyImg = PROJECT_IMAGES + '/projectImages/fitnessbuddy.png'
+    pacmanImg = PROJECT_IMAGES + '/projectImages/pacman.png'
+    calcImg = PROJECT_IMAGES + '/projectImages/calc.png'
+    satSolverImg = PROJECT_IMAGES + '/projectImages/satsolver.png'
+    shellImg = PROJECT_IMAGES + '/projectImages/shell.png'
+    voluntutorImg = PROJECT_IMAGES + '/projectImages/voluntutor.png'
+}
