36e5f4f2e5cd34934c034d74c432d537a906e6c0
Lee Roy Gomos <leeroy.gomos@gmail.com>
2022-03-18 01:24:42 -0700

    updated experience and skills page

--- force-app/main/default/lwc/skillsCertifications/skillsCertifications.js
+++ force-app/main/default/lwc/skillsCertifications/skillsCertifications.js
@@ -1,3 +1,7 @@
 import { LightningElement } from 'lwc';
+import CERTS from '@salesforce/resourceUrl/certs';
 
-export default class SkillsCertifications extends LightningElement {}
+export default class SkillsCertifications extends LightningElement {
+    certAdmin = CERTS + '/certs/admin.png';
+    certPD1 = CERTS + '/certs/pd1.png';
+}
